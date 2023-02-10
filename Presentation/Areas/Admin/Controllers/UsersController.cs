5aa028509915f1393e2c93d4505ffbbc1a47c75b
Denis Krasniqi <denis_me6@hotmail.com>
2023-02-10 19:37:30 +0100

    Add function and Edit function added

--- Presentation/Areas/Admin/Controllers/UsersController.cs
+++ Presentation/Areas/Admin/Controllers/UsersController.cs
@@ -47,26 +47,101 @@ namespace Presantation.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult Edit(string id)
         {
-            var model = new UserViewModel();
-            model.Id = id;
-            return View("Add",model);
+            AspNetUser? user = userRepository.GetByStringId(id);
+            if (user != null)
+            {
+                var model = new UserViewModel()
+                {
+                    Id = id,
+                    Password = "",
+                    ConfirmPassword = "",
+                    Email = user.Email!,
+                    EmailConfirmed = user.EmailConfirmed,
+                    Name = user.Name!,
+                    PhoneNumber = user.PhoneNumber,
+                    PhoneNumberConfirmed = user.PhoneNumberConfirmed,
+                    RoleId = rolesRepository.GetByUserId(user.Id)!.Id,
+                    Surname = user.Surname!,
+                };
+
+                //model.Roles = new SelectList(selectListService.GetRolesKeysValues(), "SKey", "Value", model.RoleId);
+
+                return View("Add", model);
+            }
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
        
-        public IActionResult Add(UserViewModel model)
+        public async Task<IActionResult> AddAsync(UserViewModel model)//Method for adding new user
         {
             if(ModelState.IsValid)
             {
+                string userId = "";
+                if (string.IsNullOrEmpty(model.Id))
+                {
+                    var user = new ApplicationUser { Name = model.Name, Surname = model.Surname, UserName = model.Email, Email = model.Email, EmailConfirmed = model.EmailConfirmed, PhoneNumber = model.PhoneNumber, PhoneNumberConfirmed = model.PhoneNumberConfirmed };
+                    var result = await _userManager.CreateAsync(user, model.Password);
+                    if (result.Succeeded)
+                    {
+                        userId = user.Id;
+                        var selectedRole = rolesRepository.GetByStringId(model.RoleId);
+                        if (selectedRole != null)
+                        {
+                            var roleResult = await _userManager.AddToRoleAsync(user, selectedRole.Name);
+                            if (roleResult.Succeeded)
+                            {
+                                _logger.LogInformation($"User created with role {selectedRole.Name}");
+                            }
+                        }
+
+                        _logger.LogInformation("User created a new account with password.");
+                    }
+                    
+                }
+                else
+                {
+                    var user = await _userManager.FindByIdAsync(model.Id);
+                    if (user != null)
+                    {
+                        user.Name = model.Name;
+                        user.Surname = model.Surname;
+                        //user.Email = model.Email;
+                        user.EmailConfirmed = model.EmailConfirmed;
+                        user.PhoneNumber = model.PhoneNumber;
+                        user.PhoneNumberConfirmed = model.PhoneNumberConfirmed;
+
+                        var editResult = await _userManager.UpdateAsync(user);
+
+                        if (editResult.Succeeded)
+                        {
+                            var currentRole = rolesRepository.GetByUserId(user.Id);
+                            if (currentRole != null && currentRole.Id != model.RoleId)
+                            {
+                                var result = await _userManager.RemoveFromRoleAsync(user, currentRole.Name);
+                                if (result.Succeeded)
+                                {
+                                    var selectedRole = rolesRepository.GetByStringId(model.RoleId);
+                                    if (selectedRole != null)
+                                    {
+                                        await _userManager.AddToRoleAsync(user, selectedRole.Name);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
 
             }
+            
             return RedirectToAction("Index");
         }
 
         [HttpGet]
         public IActionResult GetUsersJson()
         {
-            var users = userRepository.GetAll();
+            var users = userRepository.GetAllWithRoles();
             //users.ForEach(x => x.PasswordHash = userRepository.GetProfilePicturePath(x.Id, (int)ThumbnailsEnum.Grid));
 
             try
@@ -77,7 +152,8 @@ namespace Presantation.Areas.Admin.Controllers
                     name = x.Name,
                     surname = x.Surname,
                     email = x.Email,
-                    EmailConfirmed = x.EmailConfirmed
+                    EmailConfirmed = x.EmailConfirmed,
+                    role = x.Roles.FirstOrDefault()!.Name
                 });
 
                 return new JsonResult(result);
